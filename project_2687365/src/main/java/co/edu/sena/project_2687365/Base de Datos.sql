298ce217fa0e25bff4b924001d3a83bcdb8d0341
Nicol1921 <128154864+Nicol1921@users.noreply.github.com>
2023-06-06 08:06:10 -0500

    Update Base de Datos.sql

--- project_2687365/src/main/java/co/edu/sena/project_2687365/Base de Datos.sql
+++ project_2687365/src/main/java/co/edu/sena/project_2687365/Base de Datos.sql
@@ -7,6 +7,16 @@ user_lastname varchar(40) not null,
 user_email varchar(60)not null,
 user_password varbinary(255)
 );
+create table category_tbl(
+category_id int not null primary key auto_increment,
+category_name VARCHAR(40) not null
+);
+create table product_tbl(
+product_id int not null primary key auto_increment,
+product_name VARCHAR(40) not null,
+product_value int null,
+category_id int not null
+);
 select * from users_tbl;
 
 insert into users_tbl (user_firstname, user_lastname, user_email, user_password) values ("Nicol","Garcia","buzon@email.com","123");
@@ -26,7 +36,5 @@ add constraint category_id
 foreign key (category_id)
 references category_tbl (category_id);
 
-insert INTO CarUnity.users_tbl (user_firstname, user_lastname, user_email, user_password)VALUES (UPPER('fabian'), UPPER('perez'), 'mate@gmail.com',AES_ENCRYPT('password','$2a$15$Ov6xYXkMlR2Ez2/RwUiO.OoYBEIKcQzy3YQkIfeIQXCiTPUSAWPta'));
+insert INTO users_tbl (user_firstname, user_lastname, user_email, user_password)VALUES (UPPER('fabian'), UPPER('perez'), 'mate@gmail.com',AES_ENCRYPT('password','$2a$15$Ov6xYXkMlR2Ez2/RwUiO.OoYBEIKcQzy3YQkIfeIQXCiTPUSAWPta'));
 SELECT *,CAST(AES_DECRYPT(user_password,'$2a$15$Ov6xYXkMlR2Ez2/RwUiO.OoYBEIKcQzy3YQkIfeIQXCiTPUSAWPta') AS CHAR(50))end_data FROM users_tbl WHERE user_id = 1;
-
-						
